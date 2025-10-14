f8e89071527bade67dd0ef3c783b97e744136358
pirogovsasha11 <pirogovsasha11@gmail.com>
2025-10-14 23:00:50 +0300

    Задание 3 ошибка исправлена

--- lesson_01/lesson_1_task_3.py
+++ lesson_01/lesson_1_task_3.py
@@ -1,4 +1,4 @@
 print('Как вас зовут ?')
-first_name = input()
 last_name = input()
-print('Вас зовут, ' + first_name + ' ' + last_name)
+first_name = input()
+print('Вас зовут, ' + last_name + ' ' + first_name)
