6c886a178801651a6410bb8dd39466e463e174eb
pirogovsasha11 <pirogovsasha11@gmail.com>
2025-10-21 22:13:42 +0300

    Второе домашнее задание

---
+++ training/training_1.py
@@ -0,0 +1,25 @@
+my_height = 183
+print(my_height)
+
+my_name = "Alexander"
+my_name = "Alexander_Pirogov"
+print(my_name)
+
+#print('Как зовут вашего питомца?')
+#pet_name = input()
+#print('Мой питомец, ' + pet_name)
+
+def print_python():
+    print("Учу Python!")
+print_python()
+
+def print_letter(let):
+    print(let, end='')
+
+print_letter('С')
+print_letter('Т')
+print_letter('У')
+print_letter('Д')
+print_letter('Е')
+print_letter('Н')
+print_letter('Т')
